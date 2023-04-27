2492300072711e7d9a0abb032a76076ee6c410d2
David <dstumph@gmail.com>
2023-04-27 16:48:38 -0700

    Get MiniJava to use the parser

--- src/MiniJava.java
+++ src/MiniJava.java
@@ -1,47 +1,84 @@
-import java.io.File;
+import java.io.*;
 
+import AST.Program;
+import AST.Visitor.ASTDump;
+import AST.Visitor.PrettyPrintVisitor;
+import Parser.parser;
 import Parser.sym;
 import Scanner.*;
+import java_cup.runtime.ComplexSymbolFactory;
 import java_cup.runtime.Symbol;
 
-import java.io.FileReader;
 import java.util.Scanner;
 
 public class MiniJava {
+    private static void usage() {
+        System.out.println("USAGE: MiniJava -S [filename]");
+        System.exit(1);
+    }
 
     public static void main (String[] args) {
         if (args.length != 2) {
-            System.out.println("USAGE: MiniJava -S [filename]" );
-            System.exit(1);
-            return;
+            usage();
         }
 
-        if (!args[0].equals("-S")) {
-            System.out.println("USAGE: MiniJava -S [filename]");
-            System.exit(1);
-            return;
-        }
-
-        // System.exit doesn't work right in a try, so workaround
-        int exitCode = 0;
-        boolean valid = true;
+        FileReader f = null;
         try {
-            scanner scanner = new scanner(new FileReader(args[1]));
-            Symbol t = scanner.next_token();
-
-            while (t.sym != sym.EOF) {
-                System.out.print(scanner.symbolToString(t) + " ");
-                if (t.sym == sym.error) {
-                    valid = false;
-                }
-                t = scanner.next_token();
-            }
-        } catch (Exception e) {
-            System.err.println(e);
-            e.printStackTrace();
-            exitCode = 1;
+            f = new FileReader(args[1]);
+        } catch(Exception e) {
+            System.exit(1);
         }
-        if (!valid) exitCode = 1;
-        System.exit(exitCode);
+        ComplexSymbolFactory sf = new ComplexSymbolFactory();
+        Reader in = new BufferedReader(f);
+        scanner scanner = new scanner(in, sf);
+
+        if (args[0].equals("-S")) {
+            // Scanner
+            // System.exit doesn't work right in a try, so workaround
+            int exitCode = 0;
+            boolean valid = true;
+            try {
+                Symbol t = scanner.next_token();
+
+                while (t.sym != sym.EOF) {
+                    System.out.print(scanner.symbolToString(t) + " ");
+                    if (t.sym == sym.error) {
+                        valid = false;
+                    }
+                    t = scanner.next_token();
+                }
+            } catch (Exception e) {
+                System.err.println(e);
+                e.printStackTrace();
+                exitCode = 1;
+            }
+            if (!valid) exitCode = 1;
+            System.exit(exitCode);
+        } else if (args[0].equals("-A")) {
+            parser p = new parser(scanner, sf);
+            try {
+                Symbol sym = p.parse();
+                Program program = (Program)sym.value;
+                program.accept(new ASTDump());
+            } catch (Exception e) {
+                System.err.println(e);
+                e.printStackTrace();
+                System.exit(1);
+            }
+        } else if (args[0].equals("-P")) {
+            parser p = new parser(scanner, sf);
+            try {
+                Symbol sym = p.parse();
+                Program program = (Program)sym.value;
+                program.accept(new PrettyPrintVisitor());
+            } catch (Exception e) {
+                System.err.println(e);
+                e.printStackTrace();
+                System.exit(1);
+            }
+        } else {
+            usage();
+        }
+        System.exit(0);
     }
 }
