0d4f82123559524fdf66a46b75d374f2515b942c
David <dstumph@gmail.com>
2023-04-27 17:28:53 -0700

    Put Scanner/Keywords.java back

--- test/resources/Scanner/Keywords.java
+++ test/resources/Scanner/Keywords.java
@@ -1,14 +1,5 @@
 class Keywords {
     public static void main(String[] args) {
-        System.out.println(1);
-    }
-}
 
-class Stuff {
-    int stuff;
-    public int item(int thing) {
-        int a;
-        a = 5;
-        return 5;
     }
 }
