92fa67aeab9120cfb95c0d5792788f54a8cb2e65
David <dstumph@gmail.com>
2023-05-17 19:58:54 -0700

    Semantics test runner

---
+++ test/junit/TestSemantics.java
@@ -0,0 +1,53 @@
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static org.junit.Assert.fail;
+
+
+public class TestSemantics {
+
+    public static final String TEST_FILES_LOCATION = "test/resources/Semantics/";
+    public static final String TEST_FILES_INPUT_EXTENSION = ".java";
+    public static final String TEST_FILES_EXPECTED_EXTENSION = ".expected";
+    public static final String TEST_FILES_ERROR_EXTENSION = ".stderr";
+
+    /*
+        You may be able to reuse this private helper method for your own
+        testing of the MiniJava scanner.
+    */
+    private void runSemanticsTestCase(String testCaseName) {
+        try {
+            new MiniJavaTestBuilder()
+                    .assertSystemOutMatchesContentsOf(
+                            Path.of(TEST_FILES_LOCATION,
+                                    testCaseName + TEST_FILES_EXPECTED_EXTENSION))
+                    .testCompiler("-A", TEST_FILES_LOCATION + testCaseName + TEST_FILES_INPUT_EXTENSION);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    private void runSemanticsTestCaseWithErr(String testCaseName) {
+        try {
+            new MiniJavaTestBuilder()
+                    .assertSystemOutMatchesContentsOf(
+                            Path.of(TEST_FILES_LOCATION,
+                                    testCaseName + TEST_FILES_EXPECTED_EXTENSION))
+                    .assertSystemErrMatchesContentsOf(
+                            Path.of(TEST_FILES_LOCATION,
+                                    testCaseName + TEST_FILES_ERROR_EXTENSION))
+                    .testCompiler("-A", TEST_FILES_LOCATION + testCaseName + TEST_FILES_INPUT_EXTENSION);
+        } catch (IOException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    /*@Test
+    public void testSimple() {
+        runSemanticsTestCase("Simple");
+    }*/
+
+    
+}
